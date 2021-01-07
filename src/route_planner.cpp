ad2304d9c94ca68531a6cb38e8fbce13f7c05e7b
Daniel M Campos <daniel.m.campos@icloud.com>
2021-01-07 14:44:56 -0600

    Add missing logic
    - TODO 4

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -19,17 +19,17 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
                    std::pow(node->y - end_node->y, 2));
 }
 
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding
-// all unvisited neighbors to the open list. Tips:
-// - Use the FindNeighbors() method of the current_node to populate
-// current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the
-// g_value.
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and
-// set the node's visited attribute to true.
-
-void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {}
+void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+  current_node->FindNeighbors();
+  for (auto neighbor : current_node->neighbors) {
+    neighbor->parent = current_node;
+    neighbor->h_value = CalculateHValue(neighbor);
+    neighbor->g_value =
+        current_node->g_value + current_node->distance(*neighbor);
+    open_list.push_back(neighbor);
+    neighbor->visited = true;
+  }
+}
 
 // TODO 5: Complete the NextNode method to sort the open list and return the
 // next node. Tips:
